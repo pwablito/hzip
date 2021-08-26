83a44b0d561d5cc23e1df22e706858864b938d14
pwablito <pwablito@gmail.com>
2021-08-26 00:27:40 -0600

    Improved io utils, add reverse

--- io.c
+++ io.c
@@ -2,32 +2,46 @@
 #include "key_table.h"
 
 int write_key_table(unsigned char* buffer, struct KeyTable table) {
-    for (unsigned int i = 0; i < 0x100; i++) {
-        buffer[3 * i] = (char)i;
-        buffer[(3 * i) + 1] = lookup_bit_length(table, (char)i);
-        buffer[(3 * i) + 2] = lookup_table_value(table, (char)i);
-    }
-    // TODO return the number of bytes written
-    return 0;
+    // TODO optimize this
+    memcpy(buffer, table.table, 0x100 * sizeof(struct KeyTableEntry));
+    return (0x100 * sizeof(struct KeyTableEntry)) * 8;
+}
+
+int read_key_table(unsigned char* buffer, struct KeyTable* table) {
+    // TODO optimize this
+    memcpy(table->table, buffer, 0x100 * sizeof(struct KeyTableEntry));
+    return (0x100 * sizeof(struct KeyTableEntry)) * 8;
 }
 
 int write_compressed_buffer(unsigned char* buffer, struct KeyTable table, unsigned char* input, int size) {
-    // Write bits to output buffer
-    int bytes_written = 0;
-    unsigned char output_byte = (unsigned char) 0;
-    for (unsigned int input_buffer_position = 0; input_buffer_position < size; input_buffer_position++) {
-        int bit_length = lookup_bit_length(table, input[input_buffer_position]);
-        int value = lookup_table_value(table, input[input_buffer_position]);
-        // Write `bit_length` bits of `value` to output buffer
-        // This code doesn't work yet: needs further alignment within byte boundaries (currently writes only one table value per byte)
-        for (int i = 0; i < bit_length; i++) {
-            output_byte = (output_byte << 1) | (value & 1);
-            value = value >> 1;
+    unsigned int buffer_pos = 0;
+    unsigned int input_pos = 0;
+    unsigned char current_chunk = 0;
+    unsigned int current_chunk_filled = 0;
+    while (input_pos < size) {
+        unsigned int encoded_length = lookup_bit_length(table, input[input_pos]);
+        unsigned int encoded_value = lookup_table_value(table, input[input_pos]);
+        for (unsigned int current_value_pos = 0; current_value_pos < encoded_length; current_value_pos++) {
+            current_chunk<<=1;
+            current_chunk |= (encoded_value >> (encoded_length - current_value_pos - 1)) & 1;
+            current_chunk_filled++;
+            if (current_chunk_filled == 8) {
+                buffer[buffer_pos] = current_chunk;
+                current_chunk = 0;
+                current_chunk_filled = 0;
+                buffer_pos++;
+            }
         }
-        // Write output_byte to output buffer
-        buffer[bytes_written++] = output_byte;
-        output_byte = (unsigned char) 0;
+        input_pos++;
     }
-    return bytes_written;
+    if (current_chunk_filled > 0) {
+        buffer[buffer_pos] = current_chunk;
+        buffer_pos++;
+    }
+    return (buffer_pos * 8) + current_chunk_filled;
+}
 
+int read_compressed_buffer(unsigned char* buffer, struct KeyTable table, unsigned char* output, int size) {
+    printf("Not implemented");
+    return 0;
 }
--- io.h
+++ io.h
@@ -3,7 +3,9 @@
 
 #include "key_table.h"
 
-int write_key_table(unsigned char* buffer, struct KeyTable table); // Returns # of bytes written
-int write_compressed_buffer(unsigned char* buffer, struct KeyTable table, unsigned char* input, int size); // returns # of bytes written
+int write_key_table(unsigned char* buffer, struct KeyTable table); // Returns # of bits written
+int read_key_table(unsigned char* buffer, struct KeyTable* table); // Returns # of bits read
+int write_compressed_buffer(unsigned char* buffer, struct KeyTable table, unsigned char* input, int size); // returns # of bits written
+int read_compressed_buffer(unsigned char* buffer, struct KeyTable table, unsigned char* input, int size); // returns # of bits read
 
 #endif
